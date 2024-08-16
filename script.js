584b3ca6e967879eb44f670aaa441c330e60a927
saroshkhann <ksarosh785@gmail.com>
2024-08-16 15:21:39 +0500

    modified

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 const btn = document.querySelector(".password__button");
 const inputBox = document.querySelector(".password__input");
 const labelInput = document.querySelector(".password__icon");
+const container = document.querySelector(".container");
+const value = document.querySelector(".password__value");
 
 const length = 12;
-
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
@@ -24,9 +25,23 @@ function createPassword() {
     password += allChars[Math.floor(Math.random() * allChars.length)];
   }
   inputBox.value = password;
-  navigator.clipboard.writeText(inputBox.value);
 }
+
+const insertPassword = function () {
+  const html = ` <p class="password__value">${inputBox.value}</p>`;
+
+  container.insertAdjacentHTML("afterbegin", html);
+
+  value.textContent = "";
+
+  value.style.borderBottom = "none";
+};
 
 btn.addEventListener("click", () => {
   createPassword();
+  insertPassword();
+});
+
+labelInput.addEventListener("click", () => {
+  navigator.clipboard.writeText(inputBox.value);
 });
